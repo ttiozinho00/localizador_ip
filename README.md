00f73ab6eb2d72c6a19cbe33413efaa9988403a5
Douglas <dodo3d123@hotmail.com>
2023-01-25 13:30:30 -0400

    codigo localizado ip atualizado

---
+++ README.md
@@ -0,0 +1,14 @@
+# localizador_ip
+
+codigo feito em python rastreador de IP vrtual.
+
+
+# modo de utilizar
+
+```
+python localizador.py -i (coloque o ip)
+```
+
+# modo de ver o IP
+
+acesse https://meuip.com.br/ 
---
+++ localizador.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+from bs4 import BeautifulSoup
+import requests
+import argparse
+import urllib.request
+
+def show_result(infos):
+    show_info = (
+                "IP {0}\n".format(infos["IP Address"]) +
+                "Longitude: {0}\n".format(infos["Longitude"]) +
+                "Latitude: {0}\n".format(infos["Latitude"]) +
+                "Estado: {0}\n".format(infos["Region"]) +
+                "Cidade: {0}\n".format(infos["City"]) 
+    )
+    if 'Local Time' in infos:
+        show_info += "Horario do Local: {0}\n".format(infos["Local Time"])
+    show_info += "Pais: {0}\n".format(infos["Country Code"])
+    print (show_info)
+
+def command_line():
+    parse = argparse.ArgumentParser(
+        description="Get informations of IP Address")    
+    parse.add_argument("-i", "--ip", help="IP to get informations")    
+    args = parse.parse_args()
+    if args.ip:
+        get_data(args.ip)
+    else:
+        parse.print_help()
+
+def get_data(ip_address):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+    url = "http://www.geoiptool.com/en/?ip=" + ip_address
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        infos = search_informations(response.text)
+        return show_result(infos)
+    return None    
+
+def search_informations(data):
+    if data is not None:
+        informations = BeautifulSoup(data)
+        data_ip = informations.findAll("div", {"class": "sidebar-data hidden-xs hidden-sm"})
+        return take_informations(data_ip)
+
+def take_informations(bs_data_found):
+    data = bs_data_found[0]
+    all_informations = data.findAll("span")
+    dict_data = {}
+    i= 0
+    while i < len(all_informations):
+        key = str(all_informations[i].string).replace(":","")
+        value = str(all_informations[i+1].string)
+        dict_data[key] = value
+        i += 2
+    return dict_data
+
+command_line()
